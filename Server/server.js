2511e3e86dd4681a17dddce7e886373a7ff12d0e
levabala <levabala@ya.ru>
2017-03-28 23:27:59 +0300

    v1.1
    Roadpath + different colors for X,Y,Z

--- Server/server.js
+++ Server/server.js
@@ -9,8 +9,21 @@ var fs = require("fs");
 
 var port = 3000;
 
+var clientPage = "";
+
+function updateHtml(){
+    fs.readFile('../Client/index.html', function (err, html) {
+        if (err) 
+            throw err;             
+        clientPage = html;
+    });
+}
+
+setInterval(updateHtml,300);
+
 var server = http.createServer(function (req, res) {
-    var urlParsed = url.parse(req.url, true);
+    var urlParsed = url.parse(req.url, true);    
+    var query = urlParsed.query;
 
     console.log(req.socket.remoteAddress + ': ' + urlParsed.pathname);
 
@@ -34,13 +47,36 @@ var server = http.createServer(function (req, res) {
                     saveRoute(ip, date, postData);                    
                 });                
             }
+
+            res.statusCode = 202;
+            res.end('Got it');
             break;
         }
-        
+        case '/getTrack': {
+            var trackName = query.trackName;
+            fs.readFile('./Tracks/' + trackName, (err,track) => {
+                if (err) res.end(err);
+                else res.end(track);
+            });          
+            break;
+        }
+        case '/getTracksList': {
+            fs.readdir('./Tracks/', (err, files) => {
+                var str = "";
+                for (var f in files)
+                    str += files[f] + '|' + (fs.statSync('./Tracks/' + files[f]).size / 1000000).toFixed(1) + 'MB ';
+                res.end(str) 
+            })
+            break;
+        }
+        default: {            
+            res.writeHeader(200, {"Content-Type": "text/html"});  
+            res.write(clientPage);  
+            res.end();  
+            break;
+        }               
     }
-
-    res.statusCode = 202;
-    res.end('Got it');
+    //res.end("I'am on");
 }).listen(port, "192.168.3.6");
 
 function saveRoute(ip, date, route){
@@ -48,12 +84,13 @@ function saveRoute(ip, date, route){
     var parts = route.split('|');
     parts.pop();
     
+    
     for (var l in parts){
         var oneLine = parts[l].split(';');
         var obj = {
             position: {
-                latitude: oneLine[0],
-                longitude: oneLine[1]
+                lat: oneLine[0],
+                lng: oneLine[1]
             },
             time: oneLine[2],
             accelerations: {
